da548a4dcea0dfca91767864a20390088b93026b
Grazy222 <https://github.com/Grazy222/alura-Flix.git>
2024-09-09 18:04:31 -0300

    Criando API do Rick ainda finalizando

---
+++ dados.js
@@ -0,0 +1,52 @@
+const dados = [
+    {
+        "id": 10,
+        "name": "Close Rick-counters of the Rick Kind",
+        "episode": "https://www.youtube.com/watch?v=Yyu0fRH_w8U&ab_channel=Raiomaru",
+        "characters": [
+            "https://rickandmortyapi.com/api/character/1",
+            "https://rickandmortyapi.com/api/character/2",
+            // ...
+        ],
+        "url": "https://rickandmortyapi.com/api/episode/28",
+        "created": "2017-11-10T12:56:36.618Z"
+    },
+    {
+        "id": 28,
+        "name": "Rick Sanchez",
+        "episode": "S03E07",
+        "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        characters: [
+            "https://rickandmortyapi.com/api/character/1",
+            "https://rickandmortyapi.com/api/character/1",
+            // ...
+        ],
+        "url": "https://rickandmortyapi.com/api/episode/1",
+        "created": "2017-11-10T12:56:33.798Z"
+    },
+    {
+        "id": 46,
+        "nome": "Conta",
+        "image": "https://rickandmortyapi.com/api/character/avatar/46.jpeg",
+        "url": "",
+        "episóde": [
+            "https://rickandmortyapi.com/api/episode/2"
+        ],
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
+
--- script.js
+++ script.js
@@ -1,34 +1,82 @@
-function search() {
-    const searchInput = document.getElementById('searchInput');
-    const resultsDiv = document.getElementById('resultsDiv');
+const searchInput = document.getElementById('searchInput');
+const cardContainer = document.getElementById('cardContainer');
+
+const displayDados = dados => {
+    cardContainer.innerHTML = "";
+    dados.forEach(e => {
+        cardContainer.innerHTML += `
+        <div class="card">
+            <h2>${e.id}</h2>
+                <img src="${e.image}" alt="${e.name}">
+            <p>${e.url}</p>
+        </div>
+        `
+    })
+
 }
 
-const searchTerm = searchInput.value.toLowerCase();
 
-const categorias = {
-    futebol: ['Neymar Jr.', 'Pele', 'Vínicius Jr'],
-    volei: ['Maicon Sidnei', 'Fernando Venturini', 'Giovanna'],
-    Ginasta: ['Rebeca Andrade', 'Daiana dos Santos']
-}
+console.log(dados); // listando [2] em qual nome quero imprimir
 
-// Aqui você implementaria a lógica de busca real, consultando um banco de dados ou API.
-// Por enquanto, vamos simular resultados:
 
-let results = [];
-if (searchTerm === 'rebeca andrade' || searchTerm === 'neymar' || searchTerm === 'martins') {
-    results = ['Biografia de rebeca andrade', 'Neymar Jr. - Paris Saint-Germain', 'Martins - Atleta Olímpico'];
-} else if (searchTerm.includes('futebol')) {
-    results = ['História do futebol brasileiro', 'Copa do Mundo', 'Seleção Brasileira'];
-} else if (searchTerm.includes('olimpíadas')) {
-    results = ['Olimpíadas Rio 2016', 'Medalhas brasileiras', 'Atletas olimpícos'];
-} else {
-    results - ['Nenhum resultado encontrado para: ' + searchTerm];
-}
+window.addEventListener("load", displayDados.bind(null, dados))
 
-// Exibir os resultados
-let html = '';
-results.forEach(result => {
-    html += `<p>${result}</p>`;
-});
-resultsDiv.innerHTML = html;
+
+
+// searchInput.addEventListener('input',
+
+//     async () => {
+//         const searchTerm = searchInput.value;
+//         const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${searchTerm}`);
+//         const data = await response.json();
+//         searchResultsDiv.innerHTML = '';
+//         data.resultsDiv.forEach(character => {
+//             const div = document.createElement('div');
+//             div.inerrHTML = `
+//             <img src="${character.image}" alt="${character.name}">
+//       <h2>${character.name}</h2>
+//       <p>${character.species}</p>
+//             `
+//         })
+//     }
+// )
+
+
+
+
+
+
+// function search() {
+//     const searchInput = document.getElementById('searchInput');
+//     const resultsDiv = document.getElementById('resultsDiv');
+// }
+
+// const searchTerm = searchInput.value.toLowerCase();
+
+// const categorias = {
+//     futebol: ['Neymar Jr.', 'Pele', 'Vínicius Jr'],
+//     volei: ['Maicon Sidnei', 'Fernando Venturini', 'Giovanna'],
+//     Ginasta: ['Rebeca Andrade', 'Daiana dos Santos']
+// }
+
+// // Aqui você implementaria a lógica de busca real, consultando um banco de dados ou API.
+// // Por enquanto, vamos simular resultados:
+
+// let results = [];
+// if (searchTerm === 'rebeca andrade' || searchTerm === 'neymar' || searchTerm === 'martins') {
+//     results = ['Biografia de rebeca andrade', 'Neymar Jr. - Paris Saint-Germain', 'Martins - Atleta Olímpico'];
+// } else if (searchTerm.includes('futebol')) {
+//     results = ['História do futebol brasileiro', 'Copa do Mundo', 'Seleção Brasileira'];
+// } else if (searchTerm.includes('olimpíadas')) {
+//     results = ['Olimpíadas Rio 2016', 'Medalhas brasileiras', 'Atletas olimpícos'];
+// } else {
+//     results - ['Nenhum resultado encontrado para: ' + searchTerm];
+// }
+
+// // Exibir os resultados
+// let html = '';
+// results.forEach(result => {
+//     html += `<p>${result}</p>`;
+// });
+// resultsDiv.innerHTML = html;
 
